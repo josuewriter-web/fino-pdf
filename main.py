632ec0d5a20998d463a67f0f52d09444eb1c411b
josuewriter-web <josue.writer@gmail.com>
2026-06-03 14:04:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, Request
-from weasyprint import HTML
+from weasyprint import HTML, FontConfiguration
 
 app = FastAPI()
 
@@ -8,12 +8,13 @@ async def convertir_pdf(request: Request):
     body = await request.body()
     html_text = body.decode("utf-8")
     
-    # Python le inyecta la fuente al texto automáticamente
+    font_config = FontConfiguration()
+    
     fuente_emojis = """
     <style>
     @font-face {
         font-family: 'MisEmojis';
-        src: url('https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/fonts/NotoColorEmoji.ttf');
+        src: url('NotoColorEmoji-Regular.ttf');
     }
     *, body, table, td, th, span {
         font-family: 'Arial', 'MisEmojis', sans-serif !important;
@@ -22,5 +23,5 @@ async def convertir_pdf(request: Request):
     """
     html_final = fuente_emojis + html_text
     
-    pdf_bytes = HTML(string=html_final).write_pdf()
+    pdf_bytes = HTML(string=html_final, base_url=".").write_pdf(font_config=font_config)
     return Response(content=pdf_bytes, media_type="application/pdf")
